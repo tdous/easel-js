0c15234b88f707d82732f7c85bd2f8f6266adb2d
Tom <tom.dancer@gmail.com>
2019-12-18 13:43:54 +0000

    "close" flag to auto-complete a shape

--- lib/draw/line.d.ts
+++ lib/draw/line.d.ts
@@ -1,6 +1,6 @@
 import { ModeStrs } from './mode';
 interface LineInterface {
-    (cx: CanvasRenderingContext2D, points: number[][], mode: ModeStrs): void;
+    (cx: CanvasRenderingContext2D, points: number[][], mode: ModeStrs, close?: Boolean): void;
 }
 export declare const line: LineInterface;
 export {};
--- src/draw/line.ts
+++ src/draw/line.ts
@@ -4,15 +4,19 @@ interface LineInterface {
   (
     cx: CanvasRenderingContext2D,
     points: number[][],
-    mode: ModeStrs
+    mode: ModeStrs,
+    close?: Boolean 
   ): void;
 }
 
-export const line: LineInterface = (cx, points, mode): void => {
+export const line: LineInterface = (cx, points, mode, close): void => {
   cx.beginPath();
   cx.moveTo(points[0][0], points[0][1]);
   for (let i = 1; i < points.length; i++) {
     cx.lineTo(points[i][0], points[i][1]);
+  }
+  if (close) {
+    cx.lineTo(points[0][0], points[0][1]);
   }
   drawWithMode(cx, mode);
 };
