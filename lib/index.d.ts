3ac6874ef3571d168b91e0bfab1102e301f9988f
Tom <tom.dancer@gmail.com>
2019-12-18 13:44:43 +0000

    Easier canvas config

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -5,15 +5,22 @@ interface CanvasInterface {
     w: number | null;
     rt: ClientRect | null;
 }
+declare type OptsType = {
+    focus: boolean;
+    tabIndex: number;
+};
 export declare class Easel implements CanvasInterface {
     h: number;
     w: number;
     cv: HTMLCanvasElement;
     cx: CanvasRenderingContext2D;
     rt: ClientRect;
-    constructor(id: string);
-    bindTo(id: string): this;
-    setupCanvas(): void;
+    constructor(id: string, opts?: OptsType);
+    bindTo(id: string, opts?: OptsType): this;
+    setupCanvas(opts?: OptsType): void;
+    config(conf: {
+        [key: string]: number | string;
+    }): void;
     wipe(): void;
 }
 export {};
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,11 @@ interface CanvasInterface {
   rt: ClientRect | null;
 }
 
+type OptsType = {
+  focus: boolean;
+  tabIndex: number;
+};
+
 export class Easel implements CanvasInterface {
   h!: number;
   w!: number;
@@ -13,24 +18,39 @@ export class Easel implements CanvasInterface {
   cx!: CanvasRenderingContext2D;
   rt!: ClientRect;
 
-  constructor(id: string) {
-    return this.bindTo(id);
+  constructor(id: string, opts?: OptsType) {
+    return this.bindTo(id, opts);
   }
 
-  bindTo(id: string) {
+  bindTo(id: string, opts?: OptsType) {
     this.cv = <HTMLCanvasElement>document.getElementById(id);
     this.cx = this.cv.getContext('2d')!;
-    this.setupCanvas();
+    this.setupCanvas(opts);
     return this;
   }
 
-  setupCanvas() {
+  setupCanvas(opts?: OptsType) {
+    if (opts) {
+      if (typeof opts.tabIndex !== 'undefined') {
+        this.cv.tabIndex = opts.tabIndex;
+      }
+      if (opts.focus) {
+        this.cv.focus();
+      }
+    }
+
     const cvStyle = window.getComputedStyle(this.cv);
     this.cv.setAttribute('width', cvStyle.width!);
     this.cv.setAttribute('height', cvStyle.height!);
     this.w = parseInt(cvStyle.width!);
     this.h = parseInt(cvStyle.height!);
     this.rt = this.cv.getBoundingClientRect();
+  }
+
+  config(conf: { [key: string]: number | string }) {
+    for (let i in conf) {
+      (this.cx as any)[i] = conf[i];
+    }
   }
 
   wipe() {
